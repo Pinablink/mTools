208710cd073d6bf847f6fffbab358c906db32ef5
PINABLINK - WEBER <pinablink@gmail.com>
2021-12-23 18:11:26 -0300

    Atualização do comando clear cmd do Linux

--- mutil/lingue/command/cclearcmdl.go
+++ mutil/lingue/command/cclearcmdl.go
@@ -1,6 +1,9 @@
 package command
 
-import "fmt"
+import (
+	"os"
+	"os/exec"
+)
 
 type CClearCmdl struct {
 }
@@ -9,7 +12,9 @@ func NewCClearCmdl() *CClearCmdl {
 	return &CClearCmdl{}
 }
 
-//
+// ExecuteCommand Implementa a forma como o linux interpreta a solicitação de comando, para limpeza do cmd
 func (ref *CClearCmdl) ExecuteCommand(param ...string) {
-	fmt.Println("Executando comando de limpeza do Linux")
+	refcmd := exec.Command("clear")
+	refcmd.Stdout = os.Stdout
+	refcmd.Run()
 }
